a38914b2819b1ced72f31df2023b63a13d886be1
Kirikuu <exito2712631@hotmail.com>
2025-03-14 19:19:42 -0500

    Introducción a Python

---
+++ uso_de_python.py
@@ -0,0 +1,39 @@
+#Autor: Jarrinson Ocampo
+#Este es un comentario de una sola linea 
+''' esto es un comentario 
+de multiples lineas
+
+'''
+
+print("Estudiando python")
+
+#Definicion de variables
+'''
+Reglas:
+Python es sensible a minusculas y mayusculas
+no se pueden espacios
+no se puede usar palabras reservadas
+no es convenientes usar numeros como nombres de variables
+
+
+'''
+x=50
+y=15
+
+print(x+y)
+
+print(type(x))
+
+# <class 'int'>: significa que x es un numero entero
+nombre= "HARRISON"
+print(nombre)
+print(type(nombre))
+
+# 
+# <class 'str'>: significa que la variable nombre es un string (cadena) 
+print(type(x))
+
+x= 4.8 
+print(type(x))
+
+
